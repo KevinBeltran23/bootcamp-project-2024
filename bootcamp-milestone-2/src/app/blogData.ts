816854a4e9a1243a8399924dc9af7f6f192dcf0f
KevinBeltran23 <kbeltr03@DESKTOP-8C2E2R6>
2024-10-31 19:16:03 -0700

    feat: initialized next js navbar and removed unneccessary files. The app runs through next now it just does not have anything yet

---
+++ bootcamp-milestone-2/src/app/blogData.ts
@@ -0,0 +1,37 @@
+export type Blog = {
+	title: string;
+	date: string;
+	description: string;
+    image: string;
+    imageAlt: string;
+    slug: string;
+};
+
+const blogs: Blog[] = [
+	{
+		title: "First Blog Entry",
+		date: "10-18-2024",
+		description: "Hey yall. This is my first blog entry. This is mainly for testing stylization and formatting. I have added some generic images for now",
+        image: "genericImage.png",
+        imageAlt: "digitally drawn representation of a file",
+        slug: "blogs/firstBlog.html",
+	},
+	{
+		title: "A Bit More About Me",
+		date: "10-18-2024",
+		description: "Hey yall. There is nothing of significance here yet either. It is simply for testing how multiple blog entries will appear",
+        image: "genericImage.png",
+        imageAlt: "digitally drawn representation of a file",
+        slug: "blogs/moreAboutMe.html",
+	},
+	{
+		title: "Third Blog Entry",
+		date: "10-18-2024",
+		description: "This is yet another blog entry for testing my website. I want to add multiple of these so that I can sample what it will look like when populated.",
+        image: "genericImage.png",
+        imageAlt: "digitally drawn representation of a file",
+        slug: "https://example.com/blogs/how-to-build-a-react-application-in-2024",
+	},
+];
+
+export default blogs;
