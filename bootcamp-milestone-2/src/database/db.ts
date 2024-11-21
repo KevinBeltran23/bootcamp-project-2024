54331f5aaea6563ae75714b18c8f606942a8afec
KevinBeltran23 <kbeltr03@DESKTOP-8C2E2R6>
2024-11-21 14:58:02 -0800

    feat: create database folder

---
+++ bootcamp-milestone-2/src/database/db.ts
@@ -0,0 +1,36 @@
+// db.ts
+import mongoose from "mongoose";
+import Blog from "@/database/blogSchema";
+
+const url: string = process.env.MONGO_URI as string;
+let connection: typeof mongoose;
+
+/**
+ * Makes a connection to a MongoDB database. If a connection already exists, does nothing
+ * Call this function at the start of api routes and data fetches
+ * @returns {Promise<typeof mongoose>}
+ */
+const connectDB = async () => {
+  if (!connection) {
+    connection = await mongoose.connect(url);
+    return connection;
+  }
+};
+
+async function getBlogs(){
+	await connectDB() // function from db.ts before
+
+	try {
+			// query for all blogs and sort by date
+	    const blogs = await Blog.find().sort({ date: -1 }).orFail()
+			// send a response as the blogs as the message
+	    return blogs
+	} catch (err) {
+	    return null
+	}
+}
+
+export default {
+  connectDB,
+  getBlogs
+}
